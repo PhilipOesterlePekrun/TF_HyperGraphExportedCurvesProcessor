6fc8c673af858318c62bcb6993c6abe213081c28
PhilipOesterlePekrun <philipoesterlepekrun@gmail.com>
2024-04-29 16:01:57 +0200

    First commit: Program.cs is able to make a .csv of all magnitude max values and times

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Utils;
+
+
+namespace HyperGraphExportedCurvesProcessor
+{
+    public class Program
+    {
+        static string inputFile = @"29.04_accelTest.csv";
+        static string outPutFile = @"29.04_accelMagnitudesOut.csv";
+
+        static string inputFolder = @"Input\";
+        static string outputFolder = @"Output\";
+        static string templateFile = outputFolder + @"template.csv";
+
+        static void Main(string[] args)
+        {
+            string rootPath = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\";
+            Console.WriteLine(rootPath);
+
+            string[] allText = readFile(rootPath + inputFolder +inputFile);
+            Console.WriteLine(StringUtils.getInterval(allText[8],18,18+10 + 1 + 4));
+            Console.WriteLine(StringUtils.expNotationToFloat(StringUtils.getInterval(allText[8],0,10+1+4)));
+            Console.WriteLine(StringUtils.expNotationToFloat(StringUtils.getInterval(allText[15],0,10+1+4)));
+            Console.WriteLine(StringUtils.getInterval("0123456789",9, 9));
+
+
+            // // read
+            int curveCount = 0;
+            float[][] timeAndMax=new float[40][];
+            {
+                int j = 0;
+                while (j < allText.Length)
+                {
+                    float max = 0;
+                    int maxPos = 0;
+                    float maxTime = 0;
+                    int i = 0;
+                    int k = i + j;
+                    while (k < allText.Length && allText[k] != "")
+                    {
+                        string expText = StringUtils.getInterval(allText[k], 18, allText[k].Length);
+                        string timeText = StringUtils.getInterval(allText[k], 0, 18);
+                        float time = StringUtils.expNotationToFloat(timeText);
+                        //short isNegative = 0;
+                        /*if (expText[0] == '-')
+                        {
+                            isNegative = 1;
+                            Console.WriteLine("E");
+                        }*/
+                        if (StringUtils.expNotationToFloat(expText) > max && time > 0.5f)
+                        {
+                            max = StringUtils.expNotationToFloat(expText);
+                            maxPos = i;
+                            maxTime = time;
+                        }
+
+                        i++;
+                        k++;
+                    }
+                    timeAndMax[curveCount] =new float[]{maxTime,max};
+                    Console.WriteLine("max, maxPos, maxVal | i: " + max + ", " + maxPos + ", " + allText[maxPos] + " | " + i);
+                    j = k + 1;
+                    curveCount++;
+                }
+            }
+
+            { // // write
+                string outPutFilePath = rootPath + outputFolder + outPutFile;
+                File.Copy(rootPath + templateFile, outPutFilePath);
+
+                for (int i =1; i < curveCount+1; i++)
+                {
+                    File.AppendAllLines(outPutFilePath,new string[] {""});
+                    File.AppendAllLines(outPutFilePath,new string[] { "RR" + i + "," + timeAndMax[i][0]+","+ timeAndMax[i][1] });
+                }
+            }
+        }
+
+        static string[] readFile(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+}
---
+++ StringUtils.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace Utils
+{
+    public class StringUtils // Functions *partly* taken from Youtube Playlist Utility
+    {
+        public static int[] checkForIn(string checkFor, string checkIn)
+        {
+            int[] returnArray = new int[20];
+            for (int i = 0; i < returnArray.Length; i++)
+            {
+                returnArray[i] = -1;
+            }
+            string checkInTemp = checkIn;
+            int arrayPos = 0;
+            if (checkFor.Length <= checkIn.Length)
+            {
+                for (int i = 0; i <= checkIn.Length - checkFor.Length; i++)
+                {
+                    //checkInTemp = deleteInterval(checkInTemp, i, checkInTemp.Length);
+                    for (int j = 0; j < checkFor.Length; j++)
+                    {
+                        if (checkFor[j] != checkIn[i + j])
+                        {
+                            break;
+                        }
+                        if (j == checkFor.Length - 1)
+                        {
+                            returnArray[arrayPos] = i;
+                            arrayPos++;
+                        }
+                    }
+                }
+            }
+            return returnArray;
+        }
+        public static string deleteInterval(string text, int from, int to) //can be composed easily; from==0 --> char 0 deleted, to>=length --> last char deleted; from==to --> one char at that pos deleted
+        {
+            string temp = "";
+            //int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!(i >= from && i <= to))
+                {
+                    temp = temp + text[i];
+                    //temp[j] = text[i];
+                    //j++;
+                }
+            }
+            return temp;
+        }
+        public static string getInterval(string text, int from, int to) // fully inclusive
+        {
+            return deleteInterval(deleteInterval(text, 0, from - 1), to - from + 1, text.Length); // text.Length always greater so it is fine
+        }
+
+        public static float expNotationToFloat(string expText) // e.g. -2.0952600000e-02 to -0.029526f
+        {
+            int decimalsDigits = 10;
+            int expDigits = 2;
+            short isNegative = 0;
+            if (expText[0] == '-')
+            {
+                isNegative = 1;
+            }
+            string left = getInterval(expText, 0, decimalsDigits +1+isNegative);
+            string expSignString = getInterval(expText, decimalsDigits +3+ isNegative, decimalsDigits +3+ isNegative);
+            string right = getInterval(expText, decimalsDigits +4+ isNegative, decimalsDigits +5+ isNegative);
+
+            int sign = 0;
+            if (expSignString == "+")
+            {
+                sign = 1;
+            }
+            if (expSignString == "-")
+            {
+                sign = -1;
+            }
+            return float.Parse(left)*MathF.Pow(10,sign* float.Parse(right));
+        }
+    }
+}
